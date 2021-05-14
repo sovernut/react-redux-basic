94f74f73073b7cdf83ba1ec9589598d4fdce3f0b
sovernut <orenomail4@gmail.com>
2021-05-14 22:25:59 +0700

    Add toggle type

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,20 +4,29 @@ import { useSelector,useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({type: 'inc'})
   }
+
+  const increaseHandler = () => {
+    dispatch({type: 'inc_amt', amount: 10})
+  }
   const decrementHandler = () => {
     dispatch({type: 'dec'})
   }
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment With 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialstate = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialstate, action) => {
   if (action.type === "inc") {
     return {
+        ...state,
       counter: state.counter + 1,
+    };
+  }
+  if (action.type === "inc_amt") {
+    return {
+        ...state,
+      counter: state.counter + action.amount,
     };
   }
   if (action.type === "dec") {
     return {
+        ...state,
       counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+        ...state,
+      showCounter: !state.showCounter,
     };
   }
   return state;
